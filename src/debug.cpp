120939fe513d467488dbe2c5187e08653f6eb6ea
Volker Kuntner <git.dsea@tutanota.com>
2025-12-02 19:59:25 +0100

    get mouse position

--- src/debug.cpp
+++ src/debug.cpp
@@ -1,4 +1,5 @@
 #include "debug.h"
+#include "defines.h"
 #include "raylib.h"
 Debugger::Debugger(bool enable_flag) {
   if (enable_flag) {
@@ -12,7 +13,12 @@ Debugger::~Debugger() {}
 
 void Debugger::DrawDebugInformation() {
   Vector2 mouse_position = GetMousePosition();
-  DrawText(TextFormat("Mouse Position:\n  x:%.2f\n  y:%.2f", mouse_position.x,
-                      mouse_position.y),
+  DrawText(TextFormat("Real Mouse Position:\n  x:%.2f\n  y:%.2f",
+                      mouse_position.x, mouse_position.y),
            100, 100, 10, RED);
+  DrawText(TextFormat("Scaled Mouse Position:\n  x:%.2f\n  y:%.2f",
+                      mouse_position.x * Config::SCALE,
+                      mouse_position.y * Config::SCALE),
+           100, 150, 10, RED);
+  DrawCircleV((Vector2){0.0f, 0.0f}, 5.0f, RED);
 }

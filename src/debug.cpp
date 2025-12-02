d7b0af691bc6ac975252630ac904740ba39f2c5d
Volker Kuntner <git.dsea@tutanota.com>
2025-12-02 19:47:04 +0100

    get mouse position

--- src/debug.cpp
+++ src/debug.cpp
@@ -12,7 +12,7 @@ Debugger::~Debugger() {}
 
 void Debugger::DrawDebugInformation() {
   Vector2 mouse_position = GetMousePosition();
-  DrawText(TextFormat("Mouse Position:\n  x:%i\n  y:%i", mouse_position.x,
+  DrawText(TextFormat("Mouse Position:\n  x:%f\n  y:%f", mouse_position.x,
                       mouse_position.y),
            100, 100, 10, RED);
 }

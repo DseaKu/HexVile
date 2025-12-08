46befc987263fbb48ebcaf0e6fa9a3e9f450c188
Volker Kuntner <git.dsea@tutanota.com>
2025-12-08 08:39:20 +0100

    Add debug information

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,6 @@
 #include "game.h"
 #include "defines.h"
+#include "hex_tile_grid.h"
 #include "raylib.h"
 
 // Initialization
@@ -64,5 +65,22 @@ void Game::DrawDebugOverlay(bool is_enabled) {
   if (!is_enabled)
     return;
   DrawFPS(100, 50);
-  hexGrid.DrawDebugOverlay(MousePos);
+  DrawCircleV(Config::SCREEN_CENTER, 5.0f, RED);
+
+  // Draw mouse position
+  Vector2 scaled_mouse_position = {
+      (this->MousePos.x - (Config::SCREEN_WIDTH / 2.0f)) / Config::ZOOM,
+      (this->MousePos.y - (Config::SCREEN_HEIGHT / 2.0f)) / Config::ZOOM};
+
+  DrawText(TextFormat("Real Mouse Position:\n  x:%.2f\n  y:%.2f",
+                      this->MousePos.x, this->MousePos.y),
+           100, 100, 10, RED);
+
+  DrawText(TextFormat("Scaled Mouse Position:\n  x:%.2f\n  y:%.2f",
+                      scaled_mouse_position.x, scaled_mouse_position.y),
+           100, 150, 10, RED);
+
+  Vector2 tile00 = hexGrid.HexagonToPoint((Hexagon){0, 0});
+  DrawText(TextFormat("Pos of qr:0,0=%.2f %.2f", tile00.x, tile00.y), 100, 200,
+           10, RED);
 };

07e410256dd206bfa5fb2bc0bf0fd38a1c98db2b
Volker Kuntner <git.dsea@tutanota.com>
2025-12-23 10:43:22 +0100

    Rework animation data strucutre

--- includes/texture_atlas.h
+++ includes/texture_atlas.h
@@ -7,16 +7,26 @@
 #include <utility>
 
 // ==========================================
-//               32x32 Resolution
+//               Texture atlas
 // ==========================================
-// y = 0
 namespace ta {
+
+// --- Resolution ---
 constexpr int RES32_OFFSET = 100;
 constexpr int RES32 = conf::TILE_RESOLUTION;
 constexpr float RES32_F = static_cast<float>(RES32);
+constexpr int RES16 = RES32 / 2;
+constexpr float RES16_F = static_cast<float>(RES16);
+constexpr int RES8 = RES16 / 2;
+constexpr float RES8_F = static_cast<float>(RES8);
 
+// ==========================================
+//               Positioning
+// ==========================================
+// --- y = 0 ---
 // --- Player ---
 constexpr int PLAYER_X = 0;
+constexpr int PLAYER_Y = 1;
 constexpr int PLAYER_X_MAX = PLAYER_X + 12;
 
 // --- Tile ---
@@ -39,27 +49,13 @@ constexpr int UI_X_MAX = UI_X + 1;
 constexpr int NUMBER_X = UI_X_MAX;
 constexpr int NUMBER_X_MAX = 1;
 
-// y = 50
+// --- y = 50 ----
 // --- Item ---
 constexpr int ITEM_X = 0;
 constexpr int ITEM_Y = 50;
 constexpr int ITEM_X_MAX = 10;
 
-// ==========================================
-//               16x16 Resolution
-// ==========================================
-constexpr int RES16 = RES32 / 2;
-constexpr float RES16_F = static_cast<float>(RES16);
-
-// ==========================================
-//               8x8 Resolution
-// ==========================================
-constexpr int RES8 = RES16 / 2;
-constexpr float RES8_F = static_cast<float>(RES8);
-
-// ==========================================
-//               Details
-// ==========================================
+// --- Details ---
 constexpr int GRASS_DETAILS =
     BIT(detail::IDX_0) | BIT(detail::IDX_1) | BIT(detail::IDX_10);
 constexpr int WATER_DETAILS = BIT(detail::IDX_8);
@@ -72,9 +68,7 @@ inline const std::map<tile::id, int> RENDER_BIT_MASK_DETAIL = {
     {tile::DIRT, DIRT_DETAILS},
 };
 
-// ==========================================
-//               Resource
-// ==========================================
+// --- Resource ---
 constexpr int GRASS_RESOURCE = BIT(rsrc::TREE);
 constexpr int WATER_RESOURCE = 0;
 constexpr int DIRT_RESOURCE = 0;
@@ -86,23 +80,18 @@ inline const std::map<tile::id, int> RENDER_BIT_MASK_RESOURCE = {
     {tile::DIRT, DIRT_RESOURCE},
 };
 
-// ==========================================
-//               Animation
-// ==========================================
-
 // --- Animation ---
 constexpr float PLAYER_ANIMATION_SPEED = 10.0f;
 constexpr float PLAYER_ANIMATION_SPEED_IDLE = 5.0f;
 constexpr int PLAYER_WALK_MAX = 8;
-
 constexpr float TILES_ANIMATION_SPEED = 0.3f;
 
-// Don't go to low, it can corruped the font
-constexpr float NUMBER_SCALE = RES32 * 0.5f;
-
-// ==========================================
-//               Items
-// ==========================================
+// --- Number ---
+constexpr float NUMBER_SCALE =
+    RES32 * 0.5f; // Don't go to low, it can corruped the font
+//
+// --- Items ---
+constexpr std::pair<float, float> ITEM_NULL = {0, 0};
 constexpr std::pair<float, float> ITEM_SET_GRASS = {ITEM_X + 0, ITEM_Y + 0};
 constexpr std::pair<float, float> ITEM_SET_WATER = {ITEM_X + 1, ITEM_Y + 0};
 constexpr std::pair<float, float> ITEM_SET_DIRT = {ITEM_X + 2, ITEM_Y + 0};
@@ -111,12 +100,36 @@ constexpr std::pair<float, float> ITEM_AXE_STONE = {ITEM_X + 0, ITEM_Y + 1};
 constexpr std::pair<float, float> ITEM_WOOD_LOG = {ITEM_X + 1, ITEM_Y + 1};
 
 inline const std::map<item::id, std::pair<float, float>> ITEM_TEXTURE_COORDS = {
-    {item::SET_GRASS, ITEM_SET_GRASS},
-    {item::SET_WATER, ITEM_SET_WATER},
-    {item::SET_DIRT, ITEM_SET_DIRT},
-    {item::AXE, ITEM_AXE_STONE},
-    {item::WOOD, ITEM_WOOD_LOG},
+    {item::NULL_ID, ITEM_NULL},        {item::SET_GRASS, ITEM_SET_GRASS},
+    {item::SET_WATER, ITEM_SET_WATER}, {item::SET_DIRT, ITEM_SET_DIRT},
+    {item::AXE, ITEM_AXE_STONE},       {item::WOOD, ITEM_WOOD_LOG},
 };
 } // namespace ta
+
+// ==========================================
+//               Animation Data
+// ==========================================
+struct animationProperties {
+  int x;
+  int y;
+  int frameCount;
+  float speed;
+};
+namespace ad {
+// --- Player ---
+constexpr animationProperties PLAYER_WALK = {.x = ta::PLAYER_X,
+                                             .y = ta::PLAYER_Y + dir::SIZE * 0,
+                                             .frameCount = 8,
+                                             .speed = 1.0f};
+constexpr animationProperties PLAYER_IDLE = {.x = ta::PLAYER_X,
+                                             .y = ta::PLAYER_Y + dir::SIZE * 1,
+                                             .frameCount = 12,
+                                             .speed = 0.7f};
+constexpr animationProperties PLAYER_ATTACK = {.x = ta::PLAYER_X,
+                                               .y =
+                                                   ta::PLAYER_Y + dir::SIZE * 2,
+                                               .frameCount = 8,
+                                               .speed = 1.0f};
+}; // namespace ad
 
 #endif // TEXTURE_ATLAS_H
